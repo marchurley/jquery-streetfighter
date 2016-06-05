0bd75536e798893ef710a7b2942a0cd04d0e832c
marchurley <marc@swiss-magic.net>
2016-06-05 11:55:06 +0200

    Animation & Intro

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+	intro();
+	play();
+});
+
+
+function play(){
 	$(".ryu").mouseenter(function(){
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
@@ -10,6 +16,7 @@ $(document).ready(function(){
 	.mousedown(function(){
 		playHadouken();
 		$(".ryu-ready").hide();
+		$(".ryu-still").hide();
 		$(".ryu-throwing").show();
 		$(".hadouken").finish().show().animate(
 			{"left": "350px"},
@@ -24,7 +31,41 @@ $(document).ready(function(){
 		$(".ryu-throwing").hide();
 		$(".ryu-ready").show();
 	});
-});
+	
+	$(document).keydown(function ( e ) {
+		if (e.which === 88) {
+			$(".ryu-still").hide();
+			$(".ryu-ready").hide();
+			$(".ryu-cool").show();
+		}
+	})
+	.keyup(function ( e ) {
+		if (e.which === 88) {
+			$(".ryu-cool").hide();
+			$(".ryu-ready").hide();
+			$(".ryu-still").show();
+			
+		}
+	});
+}
+
+
+function intro(){
+	$(".logo-sf").fadeIn(2500, function() {
+		$(this).fadeOut(1000, function() {
+			$(".broughttoyou").fadeIn(1000, function () {
+				$(this).fadeOut(1000, function() {
+					$(".logo-jq").fadeIn(1000, function() {
+						$(this).fadeOut(1000, function() {
+							$(".instruction").fadeIn(1000);	
+						})
+					})
+				})
+			})
+		})
+	})
+}
+
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
